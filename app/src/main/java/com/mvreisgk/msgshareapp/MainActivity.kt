c86d299ca5c364dabc4992b6b9caee759094fa2c
Marcus Vinicius <marcus.vinicius@kalyst.com.br>
2023-04-29 20:29:15 -0300

    first interaction

--- app/src/main/java/com/mvreisgk/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/mvreisgk/msgshareapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.mvreisgk.msgshareapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnShowToast).setOnClickListener {
+            // Code
+            Log.i("MainActivity", "Button was clicked!")
+            Toast.makeText(this, "Button was clicked!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
