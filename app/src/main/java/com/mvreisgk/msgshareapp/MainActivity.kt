b07a3027fbb676af4bfb15410733d2f2ed2cd800
Marcus Vinicius <marcus.vinicius@kalyst.com.br>
2023-04-30 14:03:19 -0300

    intents

--- app/src/main/java/com/mvreisgk/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/mvreisgk/msgshareapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.mvreisgk.msgshareapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +13,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.btnShowToast).setOnClickListener {
+        val etUserMessage: EditText = findViewById<EditText>(R.id.etUserMessage);
+
+        val btnShowToast: Button = findViewById<Button>(R.id.btnShowToast);
+        btnShowToast.setOnClickListener {
             // Code
             Log.i("MainActivity", "Button was clicked!")
             Toast.makeText(this, "Button was clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+        val btnSendMsgToNextActivity: Button = findViewById<Button>(R.id.btnSendMsgToNextActivity);
+        btnSendMsgToNextActivity.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 }
