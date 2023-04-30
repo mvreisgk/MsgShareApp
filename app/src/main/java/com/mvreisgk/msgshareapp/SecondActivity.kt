58ef6785f4763141d618536b920030132c41b0b3
Marcus Vinicius <marcus.vinicius@kalyst.com.br>
2023-04-30 14:19:36 -0300

    explicit intent

--- app/src/main/java/com/mvreisgk/msgshareapp/SecondActivity.kt
+++ app/src/main/java/com/mvreisgk/msgshareapp/SecondActivity.kt
@@ -1,6 +1,8 @@
 package com.mvreisgk.msgshareapp
 
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
@@ -8,6 +10,14 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val bundle: Bundle? = intent.extras
+        val message = bundle!!.getString("user_message")
+
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+
+        val txvUserMessage: TextView = findViewById<TextView>(R.id.txvUserMessage)
+        txvUserMessage.text = message
     }
 
 }
