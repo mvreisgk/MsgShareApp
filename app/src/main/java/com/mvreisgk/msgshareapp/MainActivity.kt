5f2ff4b5ab7f58edb3fe6cbdb3e9a3d85b2d67e1
Marcus Vinicius <marcus.vinicius@kalyst.com.br>
2023-04-30 14:35:56 -0300

    implicit intent

--- app/src/main/java/com/mvreisgk/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/mvreisgk/msgshareapp/MainActivity.kt
@@ -29,5 +29,16 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("user_message", message)
             startActivity(intent)
         }
+
+        val btnShareToOtherApps: Button = findViewById<Button>(R.id.btnShareToOtherApps);
+        btnShareToOtherApps.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+
+            startActivity(Intent.createChooser(intent, "Share to:"))
+        }
     }
 }
